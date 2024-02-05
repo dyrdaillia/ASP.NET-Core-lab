65a0945e0566f7cfb81fd6f67c63b21626c9a8a3
Illia Dyrda <106555644+dyrdaillia@users.noreply.github.com>
2024-02-05 14:31:02 +0100

    Add files via upload

---
+++ Company.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1
+{
+    // Company.cs
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        // Додайте інші властивості за потребою
+    }
+
+    // CompaniesController.cs
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompaniesController : ControllerBase
+    {
+        private static Company companyInstance = new Company
+        {
+            Id = 1,
+            Name = "Example Company",
+            Description = "This is an example company."
+            // Ініціалізуйте інші властивості за потребою
+        };
+
+        [HttpGet("getCompanyInfo")]
+        public IActionResult GetCompanyInfo()
+        {
+            return Ok(companyInstance);
+        }
+
+        [HttpGet("getRandomNumber")]
+        public IActionResult GetRandomNumber()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 101);
+            return Ok(randomNumber);
+        }
+    }
+
+}
---
+++ Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace WebApplication1
+{
+    // Startup.cs
+    public class Startup
+    {
+        // ...
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Додайте інші сервіси, якщо потрібно
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // ...
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                // Додайте інші маршрути, якщо потрібно
+            });
+        }
+    }
+
+}
